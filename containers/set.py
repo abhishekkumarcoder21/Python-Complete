d7830b22561a196476666dcb5db602acb3f995c1
abhishekkumarcoder21 <abhishek.kumar.coder21@gmail.com>
2025-08-10 09:28:08 +0530

    initial commit

---
+++ containers/set.py
@@ -0,0 +1,10 @@
+unique_numbers=(1,2,5,7,8,4,1,2,4,5,7,2,1,5,8,4,2,1,4,5)
+print(set(unique_numbers))
+
+# set is under the curly brackets simply or can use set() function to make any container as a set
+
+unique_numbers2={1,2,5,7,8,4,1,2,4,5,7,2,1,5,8,4,2,1,4,5}
+print(unique_numbers2)
+
+print(type(unique_numbers))
+print(type(unique_numbers2))
